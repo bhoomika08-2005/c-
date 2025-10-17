9e7f4470102c72a0c2e377454db8ca7b9bd5e3f5
bhoomika08-2005 <kattabhoomika199@gmail.com>
2025-10-17 14:04:54 +0530

    Add files via upload

---
+++ big or sma;.cpp
@@ -0,0 +1,22 @@
+//Biggest of two numbers
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a and b values:");
+	scanf("%d%d",&a,&b);
+	if(b>a)
+	{
+		printf("%d is smaller",a);
+	}
+		else
+		{
+		
+	}
+	printf("%d is bigger",b);
+	{
+		return 0;
+	}
+	
+	}
+
